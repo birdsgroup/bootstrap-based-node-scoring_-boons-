c3a3ee4cf0312ca975a461caee6d37e8f5d110f4
Nilesh-Subramanian <60778368+Nilesh-Subramanian@users.noreply.github.com>
2025-11-21 13:45:12 +0530

    KEGG_Pathway_ES_code.R

--- Pathway Enrichment Analysis/KEGG_Pathway_ES_code.R
+++ Pathway Enrichment Analysis/KEGG_Pathway_ES_code.R
@@ -1,28 +1,13 @@
-
-
 Tissues <- c('Brain_Amygdala', 'Brain_Cerebellum', 'Brain_Cortex', 'Brain_Substantia_nigra', 'Kidney_Cortex', 'Lung', 'Muscle_Skeletal', 'Pancreas', 'Pituitary', 'Stomach', 'Uterus', 'Vagina','Skin_Sun_Exposed_Lower_leg','Whole_Blood','Thyroid')
-
-
 i = 1
 setwd(paste0("/kegg_pathways_deg_centrality/",Tissues[i],"/Project_mu_sigma/"))
 enrichment_results <- read.delim("enrichment_results_mu_sigma.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
-ss_res_1 <-  as.data.frame(enrichment_results[,4])
+ss_res_1 <-  as.data.frame(enrichment_results[,4])  #### Column 4 is for enrichment scores, and Column 7 is for enrichment p-values
 row.names(ss_res_1) <- enrichment_results$description
 colnames(ss_res_1) <- Tissues[i]
 
-# i = 2
-# setwd(paste0("D:/work/Sugyani_work/New_Hmps_ranks/kegg_pathways_deg_centrality/",Tissues[i],"/Project_mu_sigma/"))
-# enrichment_results <- read.delim("enrichment_results_mu_sigma.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-# 
-# ss_res_2 <-  as.data.frame(enrichment_results[,7])
-# row.names(ss_res_2) <- enrichment_results$description
-# colnames(ss_res_2) <- Tissues[i]
-
-#ddff <- merge(ss_res_1, ss_res_2, by = "row.names", all = TRUE)
 ddff <- as.data.frame(ss_res_1)
-
-
 for(i in 2:length(Tissues)){
   setwd(paste0("/kegg_pathways_deg_centrality/",Tissues[i],"/Project_mu_sigma/"))
   enrichment_results <- read.delim("enrichment_results_mu_sigma.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
@@ -32,7 +17,7 @@ for(i in 2:length(Tissues)){
   }else{
     enrichment_results <- enrichment_results
   }
-  ss_res_3 <- as.data.frame(enrichment_results[,4])
+  ss_res_3 <- as.data.frame(enrichment_results[,4])   #### Column 4 is for enrichment scores, and Column 7 is for enrichment p-values
   row.names(ss_res_3) <- enrichment_results$description
   colnames(ss_res_3) <- Tissues[i]
   
