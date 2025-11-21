a5624df11b92b28c0a3205a69ee9775cad512480
SUGYANI MAHAPATRA <94941168+MSugyani015@users.noreply.github.com>
2025-11-21 13:27:31 +0530

    Pathway Enrichment Analysis

---
+++ Pathway Enrichment Analysis/KEGG_Pathway_ES_code.R
@@ -0,0 +1,53 @@
+
+
+Tissues <- c('Brain_Amygdala', 'Brain_Cerebellum', 'Brain_Cortex', 'Brain_Substantia_nigra', 'Kidney_Cortex', 'Lung', 'Muscle_Skeletal', 'Pancreas', 'Pituitary', 'Stomach', 'Uterus', 'Vagina','Skin_Sun_Exposed_Lower_leg','Whole_Blood','Thyroid')
+
+
+i = 1
+setwd(paste0("/kegg_pathways_deg_centrality/",Tissues[i],"/Project_mu_sigma/"))
+enrichment_results <- read.delim("enrichment_results_mu_sigma.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+ss_res_1 <-  as.data.frame(enrichment_results[,4])
+row.names(ss_res_1) <- enrichment_results$description
+colnames(ss_res_1) <- Tissues[i]
+
+# i = 2
+# setwd(paste0("D:/work/Sugyani_work/New_Hmps_ranks/kegg_pathways_deg_centrality/",Tissues[i],"/Project_mu_sigma/"))
+# enrichment_results <- read.delim("enrichment_results_mu_sigma.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+# 
+# ss_res_2 <-  as.data.frame(enrichment_results[,7])
+# row.names(ss_res_2) <- enrichment_results$description
+# colnames(ss_res_2) <- Tissues[i]
+
+#ddff <- merge(ss_res_1, ss_res_2, by = "row.names", all = TRUE)
+ddff <- as.data.frame(ss_res_1)
+
+
+for(i in 2:length(Tissues)){
+  setwd(paste0("/kegg_pathways_deg_centrality/",Tissues[i],"/Project_mu_sigma/"))
+  enrichment_results <- read.delim("enrichment_results_mu_sigma.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+  x <- which(duplicated(enrichment_results$description) | duplicated(enrichment_results$description, fromLast = TRUE))
+  if(length(x) != 0){
+    enrichment_results <- enrichment_results[-x,]
+  }else{
+    enrichment_results <- enrichment_results
+  }
+  ss_res_3 <- as.data.frame(enrichment_results[,4])
+  row.names(ss_res_3) <- enrichment_results$description
+  colnames(ss_res_3) <- Tissues[i]
+  
+  
+  ddff <- merge(ddff, ss_res_3, by = "row.names", all = TRUE)
+  
+  # 2. Rename the first column back to row names
+  rownames(ddff) <- ddff$Row.names
+  ddff$Row.names <- NULL
+  
+  # 3. Replace NAs (from non-matching rows) with zero
+  ddff[is.na(ddff)] <- 0
+  
+}
+
+
+setwd("/kegg_pathways_deg_centrality/")
+write.csv(ddff,file = "KEGG_pathways_ES.csv")
