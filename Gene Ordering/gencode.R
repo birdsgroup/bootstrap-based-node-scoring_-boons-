27725549d8fb4a5bb3054958212269bc0f6cb83f
SUGYANI MAHAPATRA <94941168+MSugyani015@users.noreply.github.com>
2026-04-20 15:18:34 +0530

    Updates to Version 6.1
    bias, Centralty, Original dataset, parameter analysis

--- Gene Ordering/gencode.R
+++
@@ -1,6 +0,0 @@
-# Extract Protein-Coding Genes
-print('Extracting Protein-Coding Genes')
-gencode <- rtracklayer::import("../Original Dataset/gencode.v26.GRCh38.genes.gtf")
-gencode <- as.data.frame(gencode)
-gencode <- gencode[gencode$type == "gene" & gencode$gene_type == "protein_coding",]
-write.csv(gencode, 'gencode.csv', row.names = FALSE, sep = ',')
